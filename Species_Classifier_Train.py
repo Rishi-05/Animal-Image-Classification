2a9ea995d2ce123f15bd9bfdff8c9b170655b26d
Rishi-05 <rishichilveri14@gmail.com>
2025-03-13 00:48:35 +0530

    First Commit

---
+++ Species_Classifier_Train.py
@@ -0,0 +1,64 @@
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+# Set the environment variable to disable oneDNN custom operations
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+# Load and preprocess the data
+data_dir = "C:\\Users\\RAMESH\\Desktop\\codes\\Python Projects\\photo detector\\Image Dataset for Support Vector Machine"  # Replace the Path
+classes = ["cats", "dogs", "human", "horses"]  # Replace with your class names
+image_size = (224, 224)
+batch_size = 32
+
+datagen = ImageDataGenerator(
+    rescale=1.0 / 255,
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest",
+    validation_split=0.2,
+)
+
+train_gen = datagen.flow_from_directory(
+    data_dir,
+    target_size=image_size,
+    class_mode="categorical",
+    classes=classes,
+    batch_size=batch_size,
+    subset="training",
+)
+
+val_gen = datagen.flow_from_directory(
+    data_dir,
+    target_size=image_size,
+    class_mode="categorical",
+    classes=classes,
+    batch_size=batch_size,
+    subset="validation",
+)
+
+# Create the model
+base_model = MobileNetV2(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+predictions = Dense(len(classes), activation="softmax")(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+# Train the model
+epochs = 20
+model.fit(train_gen, epochs=epochs, validation_data=val_gen)
+
+# Save the model
+model.save("./model.h5")
+
+print(model.summary())
